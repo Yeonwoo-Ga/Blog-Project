071b1851d64f8637033fb2d59b37ec6b84a78ed4
Yeonwoo Ga <vjk4874@naver.com>
2023-01-31 15:14:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+let post = '주안 김치찌개 맛집'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color : 'red', fontSize : '20px'}}>블로그 입니다</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
